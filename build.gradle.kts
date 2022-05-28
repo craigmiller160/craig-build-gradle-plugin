a303edf36750e3ea83dd181b95c7213b1a179076
Craig Miller <craigmiller160@gmail.com>
2022-05-28 13:54:30 -0400

    Progress with logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val slf4jVersion = "1.7.36"
+val gradleToolingApiVersion = "7.4.2"
+
 plugins {
     kotlin("jvm") version "1.6.20"
 }
@@ -9,11 +12,16 @@ version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://repo.gradle.org/gradle/libs-releases")
+    }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation(gradleApi())
+    implementation("org.gradle:gradle-tooling-api:$gradleToolingApiVersion")
+    implementation("org.slf4j:slf4j-simple:$slf4jVersion")
 }
 
 tasks.test {
---
+++ src/main/kotlin/io/craigmiller160/craigbuild/gradle/plugin/model/CraigBuildModelBuilder.kt
@@ -0,0 +1,11 @@
+package io.craigmiller160.craigbuild.gradle.plugin.model
+
+import org.gradle.api.Project
+import org.gradle.tooling.provider.model.ToolingModelBuilder
+
+class CraigBuildModelBuilder : ToolingModelBuilder {
+    override fun canBuild(modelName: String): Boolean =
+        CraigBuildProject::class.java.name == modelName
+
+    override fun buildAll(modelName: String, project: Project): Any = CraigBuildProjectImpl(project)
+}
