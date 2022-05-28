c9a15b04dbe24a188b1a39f4aa8b4feb552132f6
Craig Miller <craigmiller160@gmail.com>
2022-05-28 14:30:19 -0400

    Implemented remaining logic

--- src/main/kotlin/io/craigmiller160/craigbuild/gradle/plugin/CraigBuildGradlePlugin.kt
+++ src/main/kotlin/io/craigmiller160/craigbuild/gradle/plugin/CraigBuildGradlePlugin.kt
@@ -1,3 +1,13 @@
 package io.craigmiller160.craigbuild.gradle.plugin
 
-class CraigBuildGradlePlugin {}
+import io.craigmiller160.craigbuild.gradle.plugin.model.CraigBuildModelBuilder
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
+import javax.inject.Inject
+
+class CraigBuildGradlePlugin @Inject constructor(private val registry: ToolingModelBuilderRegistry): Plugin<Project> {
+    override fun apply(target: Project) {
+        registry.register(CraigBuildModelBuilder())
+    }
+}
