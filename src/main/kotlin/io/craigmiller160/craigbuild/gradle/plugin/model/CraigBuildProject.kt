591f1954f36232a85a050a2b8aa15e82ad395a3f
Craig Miller <craigmiller160@gmail.com>
2022-05-28 13:51:37 -0400

    Setup tooling api models

--- src/main/kotlin/io/craigmiller160/craigbuild/gradle/plugin/model/CraigBuildProject.kt
+++ src/main/kotlin/io/craigmiller160/craigbuild/gradle/plugin/model/CraigBuildProject.kt
@@ -1,4 +1,7 @@
 package io.craigmiller160.craigbuild.gradle.plugin.model
 
 interface CraigBuildProject {
+    val group: String
+    val name: String
+    val version: String
 }
--- src/main/kotlin/io/craigmiller160/craigbuild/gradle/plugin/model/CraigBuildProjectImpl.kt
+++ src/main/kotlin/io/craigmiller160/craigbuild/gradle/plugin/model/CraigBuildProjectImpl.kt
@@ -1,4 +1,12 @@
 package io.craigmiller160.craigbuild.gradle.plugin.model
 
-class CraigBuildProjectImpl {
+import org.gradle.api.Project
+import java.io.Serializable
+
+class CraigBuildProjectImpl(
+    project: Project
+) : CraigBuildProject, Serializable {
+    override val group: String = project.group.toString()
+    override val name: String = project.name
+    override val version: String = project.version.toString()
 }
