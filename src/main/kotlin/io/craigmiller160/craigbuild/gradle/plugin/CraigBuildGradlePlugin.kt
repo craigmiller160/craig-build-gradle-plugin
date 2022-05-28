95cdc5a9a5e3e32ee1d78329f674187ec90027b2
Craig Miller <craigmiller160@gmail.com>
2022-05-28 14:39:33 -0400

    Trying again

--- src/main/kotlin/io/craigmiller160/craigbuild/gradle/plugin/CraigBuildGradlePlugin.kt
+++ src/main/kotlin/io/craigmiller160/craigbuild/gradle/plugin/CraigBuildGradlePlugin.kt
@@ -1,14 +1,16 @@
 package io.craigmiller160.craigbuild.gradle.plugin
 
 import io.craigmiller160.craigbuild.gradle.plugin.model.CraigBuildModelBuilder
+import javax.inject.Inject
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
-import javax.inject.Inject
 
-class CraigBuildGradlePlugin @Inject constructor(private val registry: ToolingModelBuilderRegistry): Plugin<Project> {
-    override fun apply(target: Project) {
-        println("Foo22")
-        registry.register(CraigBuildModelBuilder())
-    }
+class CraigBuildGradlePlugin
+@Inject
+constructor(private val registry: ToolingModelBuilderRegistry) : Plugin<Project> {
+  override fun apply(target: Project) {
+    println("Foo222")
+    registry.register(CraigBuildModelBuilder())
+  }
 }
